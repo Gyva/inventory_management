475bb4f196e8ac6bc44b0d0297c117c48b9c9414
Gyva <umwerechr97@gmail.com>
2024-07-23 23:45:43 +0200

    Change default database to mysql

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'inventory_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventory',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
