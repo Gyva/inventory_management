937f0ee00835ee95806fce6e61289fbdddb06641
Gyva <umwerechr97@gmail.com>
2024-07-24 11:02:11 +0200

    Create inventory views

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Equipment, Request
+from .serializers import UserSerializer, EquipmentSerializer, RequestSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class EquipmentViewSet(viewsets.ModelViewSet):
+
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+
+class RequestViewSet(viewsets.ModelViewSet):
+
+    queryset = Request.objects.all()
+    serializer_class= RequestSerializer
+    
