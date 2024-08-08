900e804a44c36774be6299df3bafebf4560d1e58
UMWERE Christian <umwerechr97@gmail.com>
2024-08-08 15:13:30 +0200

    Add change password view and serializer

--- inventory_management/inventory/tests.py
+++ inventory_management/inventory/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()  # This will get your custom User model
+
+class EquipmentTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='umwere', password='umwere_97', role='logistic_officer')
+        self.user.is_active = True
+        self.user.save()
+
+    def test_get_equipment(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+
+        response = self.client.get('/api/equipment/', HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', response.data)
+
+    def test_create_equipment(self):
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+
+        data = {
+            'name': 'Test Equipment',
+            'quantity': 10
+        }
+
+        response = self.client.post('/api/equipment/', data, HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('name', response.data)
+        self.assertEqual(response.data['name'], 'Test Equipment')
--- inventory_management/inventory/urls.py
+++ inventory_management/inventory/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, EquipmentViewSet, RequestViewSet, CustomAuthToken, UserRegistrationView, LogoutView
+from .views import UserViewSet, EquipmentViewSet, RequestViewSet, CustomAuthToken, UserRegistrationView, LogoutView, ChangePasswordView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', CustomAuthToken.as_view()),
     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
