96fbde31ca124f5dfe4776b11878f9ef6f05e38f
UMWERE Christian <umwerechr97@gmail.com>
2024-08-08 13:39:47 +0200

    update Models

--- inventory_management/inventory/models.py
+++ inventory_management/inventory/models.py
@@ -2,30 +2,29 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    ROLE_CHOICES = [
+    role_choices = (
         ('LogisticOfficer', 'Logistic Officer'),
         ('Coordinator', 'Coordinator'),
-        ('HoD', 'Head of Department'),
-    ]
-    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
+        ('HoD', 'HoD'),
+    )
+    role = models.CharField(max_length=20, choices=role_choices, default='Coordinator')
 
 class Equipment(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    quantity = models.IntegerField(default=0)
+    quantity = models.IntegerField(null=True)
+    description = models.TextField(null=True)
     date_added = models.DateTimeField(auto_now_add=True)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Request(models.Model):
-    STATUS_CHOICES = [
+    status_choices = (
         ('Pending', 'Pending'),
         ('Approved', 'Approved'),
         ('Rejected', 'Rejected'),
-    ]
+    )
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    coordinator = models.ForeignKey(User, related_name='requests', on_delete=models.CASCADE)
-    request_date = models.DateTimeField(auto_now_add=True)
+    coordinator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='requests')
     requested_quantity = models.IntegerField()
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
-    approved_by = models.ForeignKey(User, null=True, blank=True, related_name='approved_requests', on_delete=models.SET_NULL)
-
+    request_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=status_choices, default='Pending')
+    approved_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='approved_requests')
