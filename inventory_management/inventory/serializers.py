90e911819d7e46e1216f7dbee8108f4911340dc0
UMWERE Christian <umwerechr97@gmail.com>
2024-08-08 13:44:10 +0200

    Allow new users to register and set som field to read_only

--- inventory_management/inventory/serializers.py
+++ inventory_management/inventory/serializers.py
@@ -1,33 +1,43 @@
 from rest_framework import serializers
 from .models import User, Equipment, Request
 
-class UserSerializer(serializers.ModelSerializer):
-    
+class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','userrname','password','role']
-        extra_kwargs = {'password':{'write_only': True}}
+        fields = ['username', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validate_data):
-            user = User(
-               username = validate_data['username'],
-               role = validate_data['role'] 
-            )
-            
-            user.set_password(validate_data['password'])
-            
-            user.save()
-            return user
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            role=validated_data['role']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            role=validated_data['role']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class EquipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipment
-        fields = '__all__'
-
+        fields = ['id', 'name', 'quantity', 'description', 'date_added', 'added_by']
+        read_only_fields = ['added_by']
 class RequestSerializer(serializers.ModelSerializer):
-
     class Meta:
-
         model = Request
-        fields = '__all__'
-
+        fields = ['id', 'equipment', 'coordinator', 'requested_quantity', 'request_date', 'status', 'approved_by']
+        read_only_fields = ['coordinator', 'request_date', 'approved_by']
